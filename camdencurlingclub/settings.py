57cd2053e91aa1c677f10e22641afcda75c76a3f
Adam Alive <adamalive.email@gmail.com>
2024-05-29 14:44:17 +0000

    Reset DEBUG = True

--- camdencurlingclub/settings.py
+++ camdencurlingclub/settings.py
@@ -23,14 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-!)7x!x0u8ud*cq+gwo5g&g#=c^9-8*h9n63z#g@kkr2nz*r$kw'
 
 # Call from env.py
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-adamalive-camdencurling-pp6yt89mz85.ws-eu114.gitpod.io',
                 '.herokuapp.com']
