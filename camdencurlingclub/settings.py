e622ee98b987410025ad49ba1c891454425d0be2
Adam Alive <adamalive.email@gmail.com>
2024-05-29 13:27:42 +0000

    Create bookings, gallery and network apps

--- camdencurlingclub/settings.py
+++ camdencurlingclub/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookings',
+    'gallery',
     'faqs',
+    'network',
+
 ]
 
 MIDDLEWARE = [
