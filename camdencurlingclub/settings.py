9d14074728d1da222fc613c89def85862e15f3ac
Adam Alive <adamalive.email@gmail.com>
2024-05-29 10:29:35 +0000

    Add code to test deploymeent to Heroku

--- camdencurlingclub/settings.py
+++ camdencurlingclub/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!)7x!x0u8ud*cq+gwo5g&g#=c^9-8*h9n63z#g@kkr2nz*r$kw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-adamalive-camdencurling-pp6yt89mz85.ws-eu114.gitpod.io']
+ALLOWED_HOSTS = ['8000-adamalive-camdencurling-pp6yt89mz85.ws-eu114.gitpod.io',
+                '.herokuapp.com']
 
 
 # Application definition
