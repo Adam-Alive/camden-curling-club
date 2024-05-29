bef000cbbc746a8177087721f94ac048043c17f2
Adam Alive <adamalive.email@gmail.com>
2024-05-29 15:19:25 +0000

    Add csrf trusted origins

--- camdencurlingclub/settings.py
+++ camdencurlingclub/settings.py
@@ -95,6 +95,10 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io/",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
