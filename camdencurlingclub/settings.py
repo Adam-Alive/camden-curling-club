c5fa71b022316f17b0f1bd72a14cec55cd77457c
Adam Alive <adamalive.email@gmail.com>
2024-05-30 14:44:51 +0000

    Set DEBUG = False

--- camdencurlingclub/settings.py
+++ camdencurlingclub/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-adamalive-camdencurling-pp6yt89mz85.ws-eu114.gitpod.io',
                 '.herokuapp.com']
