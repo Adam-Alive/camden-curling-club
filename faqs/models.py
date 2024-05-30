b5d1a87a6b8560afdc198d39a3ab4affce70f4a6
Adam Alive <adamalive.email@gmail.com>
2024-05-30 14:39:23 +0000

    Create Faq model

--- faqs/models.py
+++ faqs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Faq(models.Model):
+    """
+    TBC Stores a single.... 
+    """
+    question = models.TextField()
+    answer = models.TextField()
+    #updated_on = models.DateTimeField(auto_now=True)
+    #profile_image = CloudinaryField('image', default='placeholder')
+
+
+    def __str__(self):
+        return self.question
