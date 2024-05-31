a0baab53573d985063e6649ef9b01075e937e648
Adam Alive <adamalive.email@gmail.com>
2024-05-31 19:58:30 +0000

    Add code to bookings/views.py and urls.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking
 
 # Create your views here.
+class BookingList(generic.ListView):
+    queryset = Booking.objects.all()
+    template_name = 'booking_list.html'
