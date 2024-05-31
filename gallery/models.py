391f69666b4d0a9270f55fe1e685681e5a0389a0
Adam Alive <adamalive.email@gmail.com>
2024-05-31 14:11:45 +0000

    Create AddGalleryImage model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class AddGalleryImage(models.Model):
+    """
+    *** TBC Stores an image and caption entry.
+    """
+    username = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name='TBC XXX'
+    )
+    gallery_image = CloudinaryField('image', default='placeholder')
+    caption = models.CharField(max_length=250, unique=True)
+    added_on = models.DateTimeField(auto_now_add=True)
+    caption_updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-added_on']
+
+    def __str__(self):
+        return f'{self.caption} | added by {self.username}'
