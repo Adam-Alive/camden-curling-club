208239e1e4ec0cce44117112a26acf898e715037
Adam Alive <adamalive.email@gmail.com>
2024-06-03 17:31:20 +0000

    Update home/views.py

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,14 @@
-from . import views
+#from . import views
+#from django.urls import path
+
+#urlpatterns = [
+#    path('', views.home_page, name='home'),
+#]
+
+# Testing TemplateView method.
 from django.urls import path
+from .views import Index
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', Index.as_view(), name='home')
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,17 @@
+#from django.shortcuts import render
+#from django.views import generic
+##from django.http import HttpResponse
+
+## Create your views here.
+
+#def home_page(request):
+#    return render(request, 'home/index.html',)
+
+# Testing TemplateView method.
 from django.shortcuts import render
-from django.views import generic
-#from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 # Create your views here.
 
-def home_page(request):
-    return render(request, 'home/index.html',)
+class Index(TemplateView):
+    template_name = 'home/index.html'
