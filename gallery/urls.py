928d54aff58f3a9b3e4ce637b3638006ca5ccf70
Adam Alive <adamalive.email@gmail.com>
2024-06-07 15:48:17 +0000

    Create view and url for adding gallery image

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GalleryImageList.as_view(), name='gallery'),
+    path('', views.add_image, name='add_image'),
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -9,3 +9,46 @@ class GalleryImageList(generic.ListView):
     queryset = GalleryImage.objects.all()
     template_name = 'galleryimage_list.html'
 
+
+
+def add_image(request):
+    if request.method == "POST":
+        gallery_form = GalleryForm(data=request.POST)
+        if gallery_form.is_valid():
+            gallery_form.save()
+            messages.add_message(
+            request, messages.SUCCESS,
+            'Image submitted and awaiting approval'
+            )
+
+    gallery_form = GalleryForm()
+
+    return render(
+        request,
+        "galleryimage_list.html",
+        {"gallery_form": gallery_form
+        },
+    )
+
+
+
+#def AddImage(request):
+#    if request.method == "POST":
+#        gallery_form = GalleryForm(data=request.POST)
+#        if gallery_form.is_valid():
+#            gallery_image = gallery_form.save(commit=False)
+#            gallery_image.username = request.user
+#            gallery_image.save()
+#            messages.add_message(
+#                request, messages.SUCCESS,
+#                'Image submitted and awaiting approval'
+#            )
+
+#    gallery_form = GalleryForm()
+#    return render(
+#        request,
+#        "galleryimage_list.html",
+#        {"gallery_form": gallery_form
+#        },
+#    )
+
