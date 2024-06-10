36ed366aa99be69f31fbb327e5c90146cf2faa64
Adam Alive <adamalive.email@gmail.com>
2024-06-10 07:25:50 +0000

    Update gallery/urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.GalleryImageList.as_view(), name='gallery'),
-    path('', views.add_image, name='add_image'),
+    path('galleryimage_list/', views.add_image, name='add_image'),
 ]
