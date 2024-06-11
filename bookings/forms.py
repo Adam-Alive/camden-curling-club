dab17fd8c7be41a4248cad0c5d23c84327f6cb99
Adam Alive <adamalive.email@gmail.com>
2024-06-11 09:50:18 +0000

    Amend bookings/views.py for testing

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,7 +1,7 @@
-#from django import forms
-#from .models import Booking
+from django import forms
+from .models import Booking
 
-#class BookingForm(forms.ModelForm):
-#    class Meta:
-#        model = Booking
-#        fields = ('date', 'sheet_time', 'wheelchair_sheet')
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('date', 'sheet_time', 'wheelchair_sheet',)
