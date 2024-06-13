d66638ce2e61c97918c424939c12e70c463b02c2
Adam Alive <adamalive.email@gmail.com>
2024-06-13 21:43:49 +0000

    Update bookings/urls.py

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.make_booking, name='make_booking'),
     path('', views.BookingList.as_view(), name='bookings'),
+    path('', views.edit_booking, name='edit_booking'),
    ]
