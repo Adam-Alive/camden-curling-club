439038b9f13e74b5f128651ae8811bfc39a416cb
Adam Alive <adamalive.email@gmail.com>
2024-06-13 17:11:30 +0000

    Add docstrings to classes in bookings/forms.py

--- bookings/forms.py
+++ bookings/forms.py
@@ -3,10 +3,19 @@ from .models import Booking
 
 
 class DateInput(forms.DateInput):
+    """
+    A widget to display a datepicker for the user
+    when making a booking.
+    """
     input_type = 'date'
 
 
 class BookingForm(forms.ModelForm):
+    """
+    Connects the Booking model to the form
+    and will populate the data fields when the
+    user submits a booking.
+    """
     class Meta:
         model = Booking
         fields = ('date', 'sheet_time', 'wheelchair_sheet',)
