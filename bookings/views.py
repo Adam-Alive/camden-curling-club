3624c8f9c0aa89dd2201016cc4a6d1f623aa0135
Adam Alive <adamalive.email@gmail.com>
2024-06-13 18:23:44 +0000

    Create edit_booking view for testing

--- bookings/views.py
+++ bookings/views.py
@@ -39,3 +39,28 @@ def make_booking(request):
         "booking_form": booking_form,
     }
     return render(request, template, context)
+
+
+@login_required
+def edit_booking(request, booking_id):
+    """
+    Test for edit booking
+    """
+    user_booking = Booking.objects.get(pk=id)
+    booking_form = BookingForm(request.POST or None)
+    if request.method == 'POST':
+        if booking_form.is_valid():
+            booking_form.instance.username = request.user           
+            booking_form.save()
+            print('It is working')
+            messages.success(
+            request, 'Thank you - your new booking is confirmed.'
+            )
+            return redirect(reverse('make_booking'))              
+   
+    template = "bookings/booking_list.html"
+    context = {
+        "user_booking": user_booking,
+        "booking_form": booking_form,
+    }
+    return render(request, template, context)
