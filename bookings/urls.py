6b025aa4c047781aa6089f006eed3baa3db86cda
Adam Alive <adamalive.email@gmail.com>
2024-06-17 20:37:58 +0000

    Corrected code to enclose Cancel button in anchor element

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('', views.make_booking, name='make_booking'),
     path('', views.BookingList.as_view(), name='bookings'),    
-    path('bookings/my_bookings/', views.my_bookings, name='my_bookings'),
-    path('bookings/edit_bookings/<booking_id>', views.edit_booking, name='edit_booking'),
-    path('bookings/delete_booking/<booking_id>', views.delete_booking, name='delete_booking'),
+    path('my_bookings/', views.my_bookings, name='my_bookings'),
+    path('edit_bookings/<booking_id>', views.edit_booking, name='edit_booking'),
+    path('delete_booking/<booking_id>', views.delete_booking, name='delete_booking'),
    ]
