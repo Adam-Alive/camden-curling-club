98d6bdbc18e32c5a0a5531a0fb9b454319865d10
Adam Alive <adamalive.email@gmail.com>
2024-06-21 08:57:17 +0000

    Add test to faqs/test_views.py

--- faqs/test_views.py
+++ faqs/test_views.py
@@ -11,3 +11,22 @@ class TestDjango(TestCase):
     def test_this_thing_works(self):
         self.assertEqual(1, 0)
 
+
+class TestFaqsViews(TestCase):
+
+       def setUp(self):
+        self.user = User.objects.create_superuser(
+            username="myUsername",
+            password="myPassword",            
+        )
+        self.faq = Faq(question="A Question",
+                        answer="An Answer"
+                    )
+        self.faq.save()
+    
+        def test_render_faq_list(self):
+            response = self.client.get(reverse('faqs'))
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b"A Question", response.content)
+            self.assertIn(b"An Answer", response.content)
+
