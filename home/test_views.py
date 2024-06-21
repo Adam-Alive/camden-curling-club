89831ab3a51039e20e83149287989f44ce661ae1
Adam Alive <adamalive.email@gmail.com>
2024-06-21 20:47:50 +0000

    Create home/test_views.py and tests

---
+++ home/test_views.py
@@ -0,0 +1,12 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your tests here.
+
+class TestHomeViews(TestCase):
+
+    def test_render_home_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html', 'base.html')
