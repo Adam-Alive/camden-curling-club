a2f5b3f9a7ead4b5beb3555687f7914485a4b0f8
Adam Alive <adamalive.email@gmail.com>
2024-06-21 14:30:15 +0000

    Update gallery/test_forms.py

--- gallery/test_forms.py
+++ gallery/test_forms.py
@@ -1,16 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 from django.urls import reverse
 from .forms import GalleryForm
 
 # Create your tests here.
-
-
-class TestDjango(TestCase):
-
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
-
 
 class TestCommentForm(TestCase): 
  
