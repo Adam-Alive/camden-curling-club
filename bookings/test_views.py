2c75c214e49c6440d97d690e0738b01330321310
Adam Alive <adamalive.email@gmail.com>
2024-06-30 12:11:04 +0000

    Update bookings/test_views.py

--- bookings/test_views.py
+++ bookings/test_views.py
@@ -32,25 +32,19 @@ class TestBookingsViews(TestCase):
 
     def test_render_make_booking_page(self):
         print(self.booking)
+        self.client.login(username='Jimmy', password='myPassword')
         response = self.client.get(reverse('make_booking'))
         self.assertEqual(response.status_code, 200)
 
-    # def test_render_my_bookings_page(self):
-    #     response = self.client.get(reverse('my_bookings'))
-    #     print(response.content)
-    #     self.assertEqual(response.status_code, 200)
-    #     # self.assertTemplateUsed(response, 'bookings/my_bookings.html')
-
-    # def test_render_edit_bookings(self):
-    #     response = self.client.get(reverse('edit_booking'))
-    #     # print(response.content)
-    #     # self.assertEqual(response.status_code, 200)
-
     def test_successful_make_booking_by_user(self):
-        response = self.client.get(reverse('make_booking'))
-        # print(response.content)
-        self.assertIn(b"Jimmy", response.content)
-        self.assertIn(b"2024-07-04", response.content)
+        self.client.login(username='Jimmy', password='myPassword')
+        response = self.client.get(reverse('my_bookings'))
+        self.assertIn(b"July 4, 2024", response.content)
         self.assertIn(b"Sheet 3 at 18.00 - 19.30", response.content)
         self.assertIn(b"Required", response.content)
         self.assertIsInstance(response.context['booking_form'], BookingForm)
+
+    def test_render_my_bookings_page(self):
+        self.client.login(username='Jimmy', password='myPassword')
+        response = self.client.get(reverse('my_bookings'))
+        self.assertEqual(response.status_code, 200)
